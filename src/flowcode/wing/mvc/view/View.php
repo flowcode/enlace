bf80bba8ed03508937f50d8e9fb27d4beaa081b9
Juan Manuel Aguero <juanma.aguero@gmail.com>
2013-08-24 10:00:40 -0300

    Change masterview to layout and htmlpurifiers add

--- src/flowcode/wing/mvc/view/View.php
+++ src/flowcode/wing/mvc/view/View.php
@@ -4,6 +4,8 @@ namespace flowcode\wing\mvc\view;
 
 use flowcode\wing\mvc\config\Config;
 use flowcode\wing\mvc\exception\ViewException;
+use HTMLPurifier;
+use HTMLPurifier_Config;
 
 /**
  * Description of View
@@ -23,17 +25,18 @@ class View implements IView {
     public function render() {
         $viewConfig = Config::get("view");
         $viewRootPath = $viewConfig["path"];
-        $settedMaster = null;
+        $settedLayout = null;
 
         /* set view data available */
         $viewData = $this->viewData;
 
+        $content = "";
         /* with master */
         $viewfile = $viewRootPath . "/" . $this->getViewName() . ".view.php";
         if (file_exists($viewfile)) {
             ob_start();
             require_once $viewfile;
-            $content = ob_get_contents();
+            $content .= ob_get_contents();
             ob_end_clean();
         } else {
             throw new ViewException($viewfile);
@@ -41,21 +44,30 @@ class View implements IView {
 
         /* default master */
         $hierarchy = explode("/", $this->getViewName());
-        $masterviews = $viewConfig["masterview"];
-        if (!is_null($masterviews[$hierarchy[0]])) {
-            $settedMaster = $masterviews[$hierarchy[0]];
+        $layouts = $viewConfig["layout"];
+        if (!is_null($layouts[$hierarchy[0]])) {
+            $settedLayout = $layouts[$hierarchy[0]];
         }
 
-        if (!is_null($settedMaster)) {
-            $masterfile = $viewRootPath . "/" . $hierarchy[0] . "/" . $settedMaster . ".view.php";
-            if (file_exists($masterfile)) {
-                require_once $masterfile;
+        if (!is_null($settedLayout)) {
+            $layoutFile = $viewRootPath . "/" . $hierarchy[0] . "/" . $settedLayout . ".view.php";
+            if (file_exists($layoutFile)) {
+                ob_start();
+                require_once $layoutFile;
+                $content .= ob_get_contents();
+                ob_end_clean();
             } else {
-                throw new ViewException($settedMaster);
+                throw new ViewException($settedLayout);
             }
-        } else {
-            echo $content;
         }
+
+        /* content cleaning */
+        $htmlPurifierConfig = HTMLPurifier_Config::createDefault();
+        $purifier = new HTMLPurifier($htmlPurifierConfig);
+        $cleanContent = $purifier->purify($content);
+
+        /* render clean content */
+        echo $cleanContent;
     }
 
     public function getViewData() {
